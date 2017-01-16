8ffbdc60054c6b93ff35f89cb6e2be60ca93646f
Jakub Stanczak <kuba0095@gmail.com>
2017-01-16 19:50:57 +0100

    Add scenes

--- client/src/main/java/app/Main.java
+++ client/src/main/java/app/Main.java
@@ -7,20 +7,38 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    private Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+
+        initLoginLayout();
+        initHomeLayout();
+    }
+
+    public void initLoginLayout() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setScene(new Scene(root, 650, 400));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public void initHomeLayout() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root, 1000, 550));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        Client client = new Client("localhost", 1234);
-        client.loop();
-//        launch(args);
+//        Client client = new Client("localhost", 1234);
+//        client.loop();
+        launch(args);
     }
 
 }
